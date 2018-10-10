af03686b162b8f3ae1768461ed5d4058bb87a09f
fcf <2998000457@qq.com>
2018-10-10 23:57:59 +0800

    添加完章节和小节阅读

--- pages/bookdetail/bookdetail.js
+++ pages/bookdetail/bookdetail.js
@@ -9,6 +9,13 @@ Page({
    */
   data: {
     novelImag:'',
+    novelId:0,
+    list: [],
+
+    open: false,
+    mark: 0,
+    newmark: 0,
+    istoright: true
   },
 
   /**
@@ -19,56 +26,95 @@ Page({
     let that = this;
     let data = app.globalData.selectBook;
     that.setData({
-      novelImag: data.novel_cover
+      novelImag: data.novel_cover,
+      novelId: data.novel_id,
     });
+    
+    this.loadChapters(data.novel_id)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  loadChapters: function (id) {
+    var that = this;
+    this.setData({
+      hidden: false
+    })
+    wx.request({
+      url: 'http://132.232.54.132:8081/api/v1/novels/chapters?novel_id=' + id,
+
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          list: res.data.info,
+          hidden: true
+        });
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+
+  tap_ch: function (e) {
+    if (this.data.open) {
+      this.setData({
+        open: false
+      });
+    } else {
+      this.setData({
+        open: true
+      });
+    }
+  },
+  tap_start: function (e) {
+    // touchstart事件
+    this.data.mark = this.data.newmark = e.touches[0].pageX;
+  },
+  tap_drag: function (e) {
+    // touchmove事件
+
+    /*
+     * 手指从左向右移动
+     * @newmark是指移动的最新点的x轴坐标 ， @mark是指原点x轴坐标
+     */
+    this.data.newmark = e.touches[0].pageX;
+    if (this.data.mark < this.data.newmark) {
+      this.istoright = true;
+    }
+    /*
+     * 手指从右向左移动
+     * @newmark是指移动的最新点的x轴坐标 ， @mark是指原点x轴坐标
+     */
+    if (this.data.mark > this.data.newmark) {
+      this.istoright = false;
+
+    }
+    this.data.mark = this.data.newmark;
+
+  },
+  tap_end: function (e) {
+    // touchend事件
+    this.data.mark = 0;
+    this.data.newmark = 0;
+    if (this.istoright) {
+      this.setData({
+        open: true
+      });
+    } else {
+      this.setData({
+        open: false
+      });
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  //点击事件
+  bindItemTap: function (event) {
+    var novelId = event.currentTarget.dataset.data.novel_id; // 当前id
+    var chapterNo = event.currentTarget.dataset.data.chapter_num; //
+    wx.navigateTo({
+      url: '../readContent/readContent?novelId=' + novelId + '&chapterNo=' + chapterNo
+    });
   }
+
 })
---
+++ pages/readContent/readContent.js
@@ -0,0 +1,48 @@
+// pages/readContent/readContent.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    novelId: 0,
+    chapterNo:0,
+    info:{}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    console.log(options)
+    that.setData({
+      chapterNo: options.chapterNo,
+      novelId: options.novelId,
+    });
+    that.loadData(options.novelId,options.chapterNo)
+  },
+
+  loadData: function (novelId, chapterNo) {
+    var that = this;
+    // http://132.232.54.132:8081/api/v1/novels/special/chapter?novel_id=1&chapter_num=2
+    var url = 'http://132.232.54.132:8081/api/v1/novels/special/chapter?novel_id=' + novelId + '&chapter_num' + chapterNo;
+    wx.request({
+      url: url,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          info: res.data.info
+        });
+        wx.setNavigationBarTitle({
+          title: res.data.info.chapter_name
+        })
+      }
+    })
+  }
+
+})
